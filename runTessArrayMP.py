27e2c353592a6d5063b1f57ac6d62047df12d754
Ubuntu <ubuntu@ip-172-31-2-159.ec2.internal>
2024-06-18 15:46:45 +0000

    hdf5 results saving attributes

--- runTessArrayMP.py
+++ runTessArrayMP.py
@@ -19,7 +19,7 @@ def run_simulation(Nr_idx):
     
     ant_array.generateRPSPositions(fmax=6e9, r=1, Nrps=3)
     ant_array.excite_idx = idx
-    ant_array.initDipoleElements(freq, orientation='z')
+    ant_array.initDipoleElements(freq, orientation='y')
     tess_array = ant_array.getNearestNeighborSA(idx, Nr)
     
     theta = np.linspace(0, np.pi, 181)
@@ -34,10 +34,13 @@ def run_simulation(Nr_idx):
     nids = [el.id for el in sim.array.elements]
     print(nids)
     
-    np.savetxt(f'{results_dir}/sm{idx}.txt', (nids, sim.smn[:, 151]))
-    np.savetxt(f'{results_dir}/s11_{idx}.txt', (sim.sfreqs, sim.s11))
+    #np.savetxt(f'{results_dir}/sm{idx}.txt', (nids, sim.smn[:, 151]))
+    #np.savetxt(f'{results_dir}/s11_{idx}.txt', (sim.sfreqs, sim.s11))
     # Save HDF5 results
     with h5py.File(f'{results_dir}/element{idx}.hdf5','w') as f:
+        f.attrs['x'] = sim.array.elements[0].x
+        f.attrs['y'] = sim.array.elements[0].y
+        f.attrs['ant_type'] = sim.array.elements[0].ant_type
         smn = f.create_dataset('smn',data=sim.smn,compression='gzip')
         sfreqs = f.create_dataset('sfreqs',data=sim.sfreqs,compression='gzip')
         eth = f.create_dataset('eth',data=sim.eth,compression='gzip')
@@ -57,5 +60,5 @@ Nrs = [4,6,8,10,12,14,16,18,20,24,28,32,36]
 Nr_idx_list = [(Nr, idx) for Nr in Nrs for idx in range(37)]
 
 # Create a Pool of workers to run simulations in parallel
-with Pool() as pool:
+with Pool(int(os.cpu_count()/4)) as pool:
     pool.map(run_simulation, Nr_idx_list)
